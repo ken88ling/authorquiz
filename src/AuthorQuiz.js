9b7e8dc3d7c2175ce0d817da97d912d570bb874b
Ken Ling <ken88ling@gmail.com>
2018-11-14 05:06:30 +0800

    Module 4 add picture , update css ,  hero , books ,turn , add underscore library

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,6 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-export default function AuthorQuiz(props) {
-  return <div>{props.testProp}</div>;
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book writer by the Author shown</p>
+      </div>
+    </div>
+  );
 }
+
+function Book({ title }) {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+function Turn({ author, books }) {
+  return (
+    <div className="row turn" styles={{ backgroundColor: "White" }}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author" />
+      </div>
+      <div className="col-6">
+        {books.map(title => (
+          <Book title={title} key={title} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Continue() {
+  return <div />;
+}
+
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from{" "}
+          <a href="http://commons.wikimedia.org/wiki/Main_Page">
+            Wikemedia Commons
+          </a>{" "}
+          and are in the public domain
+        </p>
+      </div>
+    </div>
+  );
+}
+function AuthorQuiz({ turnData }) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} className="answer" />
+      <Continue />
+      <Footer />
+    </div>
+  );
+}
+
+export default AuthorQuiz;
